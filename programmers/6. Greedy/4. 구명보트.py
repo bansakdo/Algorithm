c641958a859aa54e59a760dc3f5aa45e9c828a93
bansakdo <bansakdo@gmail.com>
2021-05-08 00:37:41 +0900

    Update Question Programmers - Greedy: Rescue Boat

--- programmers/6. Greedy/4. 구명보트.py
+++ programmers/6. Greedy/4. 구명보트.py
@@ -7,33 +7,15 @@ def solution(people, limit):
     people.sort(reverse=True)
     lt, rt = 0, len(people) - 1
 
-    for i, first in enumerate(people):
-        surplus = limit - first
-        lt = i + 1
-        if surplus >= people[rt]:
+    while lt <= rt:
+        if limit >= people[lt] + people[rt]:
             rt -= 1
+        lt += 1
         answer += 1
-        if lt > rt:
-            break
 
     return answer
 # '''
 
-# 다른사람 풀이
-'''
-def solution(people, limit) :
-    answer = 0
-    people.sort()
-
-    a = 0
-    b = len(people) - 1
-    while a < b :
-        if people[b] + people[a] <= limit :
-            a += 1
-            answer += 1
-        b -= 1
-    return len(people) - answer
-'''
 
 people = [70, 50, 80, 50]
 limit = 100
