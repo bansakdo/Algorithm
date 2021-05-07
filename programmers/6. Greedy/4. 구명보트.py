dee867bc1daf02aafaa5204585ae034172051f95
bansakdo <bansakdo@gmail.com>
2021-05-08 00:23:02 +0900

    Update Question Programmers - Greedy: Rescue Boat

--- programmers/6. Greedy/4. 구명보트.py
+++ programmers/6. Greedy/4. 구명보트.py
@@ -1,70 +1,39 @@
-from collections import deque
 
-'''
-def solution(people, limit):
-    answer = 0
-    people.sort()
-
-    while people:
-        first = people.pop()
-        surplus = limit - first
-        second = -1
-        for idx, p in enumerate(people):
-            if p <= surplus:
-                second = idx
-        if second != -1:
-            people.pop(second)
-        answer += 1
-    return answer
-'''
-
-
-'''
-def solution(people, limit):
-    answer = 0
-
-    people.sort()
-    ch = [0] * len(people)
-
-    # while any(c == 0 for c in ch):
-    for i, first in enumerate(people):
-        if ch[i] == 1:
-            continue
-        surplus = limit - first
-        ch[i] = 1
-        for idx in range(len(people) - 1, -1, -1):
-            if ch[idx] == 0 and people[idx] <= surplus:
-                ch[idx] = 1
-                break
-        answer += 1
-
-    return answer
-'''
-
+# 내 풀이
+# '''
 def solution(people, limit):
     answer = 0
 
     people.sort(reverse=True)
-    ch = [0] * len(people)
+    lt, rt = 0, len(people) - 1
 
-    # while any(c == 0 for c in ch):
     for i, first in enumerate(people):
-        if ch[i] == 1:
-            continue
         surplus = limit - first
-        ch[i] = 1
-        # second = -1
-        for idx in range(len(people) - 1, -1, -1):
-            if ch[idx] == 0 and people[idx] <= surplus:
-                # second = idx
-                ch[idx] = 1
-                break
-        # if second != -1:
-        #     people[second]
+        lt = i + 1
+        if surplus >= people[rt]:
+            rt -= 1
         answer += 1
-
+        if lt > rt:
+            break
 
     return answer
+# '''
+
+# 다른사람 풀이
+'''
+def solution(people, limit) :
+    answer = 0
+    people.sort()
+
+    a = 0
+    b = len(people) - 1
+    while a < b :
+        if people[b] + people[a] <= limit :
+            a += 1
+            answer += 1
+        b -= 1
+    return len(people) - answer
+'''
 
 people = [70, 50, 80, 50]
 limit = 100
