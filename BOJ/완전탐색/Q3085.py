a8348f93e2d82ff75ea4f0843f17f775eeed2386
bansakdo <bansakdo@gmail.com>
2021-07-05 15:08:56 +0900

    Update .gitignore

--- BOJ/완전탐색/Q3085.py
+++ BOJ/완전탐색/Q3085.py
@@ -1,12 +1,58 @@
+import sys
+from collections import Counter
+
+def check(r_s, r_e, c_s, c_e):
+    global rst
+    # if not reverse:
+    #     __matrix = matrix
+    # else
+    #     __matrix = r_matrix
+    for r in range(r_s, r_e + 1):
+        row = matrix[r]
+        rst = max(rst, max(Counter(row).values()))
+
+    for r in range(r_s, r_e + 1):
+        row = matrix[r]
+        rst = max(rst, max(Counter(row).values()))
+
+
+N = int(input())
+matrix = list(input() for _ in range(N))
+r_matrix = list(map(list, zip(*matrix)))
+rst = 0
+
+# print(list(Counter(matrix[0]).items()))
+
+# for r in matrix:
+#     if r.count(r[0]) == N:
+#         print(N)
+#         sys.exit()
+# for r in r_matrix:
+#     if r.count(r[0]) == N:
+#         print(N)
+#         sys.exit()
+
+print(*r_matrix)
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+for x in range(N-1):
+    for y in range(N-1):
+        for i in range(4):
+            n_x = x + dx[i]
+            n_y = y + dy[i]
+            if 0 <= n_x < N and 0 <= n_y < N and matrix[x][y] != matrix[n_x][n_y]:
+                matrix[x][y], matrix[n_x][n_y] = matrix[n_x][n_y], matrix[x][y]
+                # if i % 2 == 0:
+                    
 
 
 
-tot = 0
-__list = list(map(int, input().split()) for _ in range(11))
-for D, V in __list:
-    tot += D + (20 * V)
 
-print(tot)
+
+
+
 
 
 
