f90673a3ae9b54f4c6a73d7945d37dbe0d40ef51
bansakdo <bansakdo@gmail.com>
2021-07-28 13:11:57 +0900

    Update BOJ - Dynamic Programming

---
+++ BOJ/Dynamic Programming/Q2193.py
@@ -0,0 +1,12 @@
+
+
+
+N = int(input())
+
+arr = [1, 1, 2, 3]
+if N <= len(arr):
+    print(arr[N-1])
+else:
+    for _ in range(N-len(arr)):
+        arr.append(arr[-1] + arr[-2])
+    print(arr[-1])
