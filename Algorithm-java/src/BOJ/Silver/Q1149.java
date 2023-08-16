bceedf58d688544bf76cea367aefb56f02a03d3b
sangdopark <bansakdo@gmail.com>
2023-08-16 13:59:21 +0900

    add BOJ Questions

--- Algorithm-java/src/BOJ/Silver/Q1149.java
+++ Algorithm-java/src/BOJ/Silver/Q1149.java
@@ -10,26 +10,58 @@ public class Q1149 {
 
         int n = Integer.parseInt(br.readLine().trim());
         int rst = Integer.MAX_VALUE;
-        int[][] order = {{0, 1, 2}, {0, 2, 1}, {1, 0, 2}, {1, 2, 0}, {2, 0, 1}, {2, 1, 0}};
+        int[][] board = new int[n][3];
 
-        int[][] costs = new int[n][3];
-        for (int i = 0; i < n; i++) {
-            costs[i] = Arrays.stream(br.readLine().trim().split(" "))
+        board[0] = Arrays.stream(br.readLine().trim().split(" "))
                     .mapToInt(Integer::parseInt)
                     .toArray();
+        for (int i = 1; i < n; i++) {
+            board[i] = Arrays.stream(br.readLine().trim().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+            board[i][0] += Math.min(board[i - 1][1], board[i - 1][2]);
+            board[i][1] += Math.min(board[i - 1][0], board[i - 1][2]);
+            board[i][2] += Math.min(board[i - 1][0], board[i - 1][1]);
         }
 
-        for (int i = 0; i < 6; i++) {
-            int val = 0;
-            for (int j = 0; j < n; j++) {
-                int idx = j % 3;
-                val += costs[j][order[j][idx]];
-            }
-            rst = Math.min(rst, val);
-        }
+        rst = Arrays.stream(board[n - 1]).min().getAsInt();
+
+
+
+
+//        int[][] order = {{0, 1, 2}, {0, 2, 1}, {1, 0, 2}, {1, 2, 0}, {2, 0, 1}, {2, 1, 0}};
+
+//        int[][] costs = new int[n][3];
+//        for (int i = 0; i < n; i++) {
+//            costs[i] = Arrays.stream(br.readLine().trim().split(" "))
+//                    .mapToInt(Integer::parseInt)
+//                    .toArray();
+//        }
+
+//        for (int i = 0; i < 6; i++) {
+//            int val = 0;
+//            for (int j = 0; j < n; j++) {
+//                int idx = j % 3;
+//                val += costs[j][order[j][idx]];
+//            }
+//            rst = Math.min(rst, val);
+//        }
 
         bw.write(String.valueOf(rst));
         bw.flush();
 
     }
 }
+/*
+3
+26 40 83
+49 60 57
+13 89 99
+
+3
+1 100 100
+100 1 100
+100 100 1
+
+*
+* */
